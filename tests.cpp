46095c255e535823ea2173841e54d06beca03dc0
RandyLu0 <lurandy8@gmail.com>
2021-10-08 12:42:56 -0400

    updated tests.cpp and main.cpp

--- tests.cpp
+++ tests.cpp
@@ -1,5 +1,21 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
-#include "funcs.h"
+#include "caesar.h"
+#include "vigenere.h"
+#include "decrypt.h"
 
-// add your tests here
+TEST_CASE("caesar cipher"){
+    CHECK(encryptCaesar("Way to Go!",5) == "Bfd yt Lt!");
+}
+
+TEST_CASE("vigenere cipher"){
+    CHECK(encryptVigenere("Hello, World!","cake") == "Jevpq, Wyvnd!");
+}
+
+TEST_CASE("decrypt caesar"){
+    CHECK(decryptCaesar("Bfd yt Lt!",5) == "Way to Go!");
+}
+
+TEST_CASE("decrypt vigenere"){
+    CHECK(decryptVigenere("Jevpq, Wyvnd!","cake") == "Hello, World!");
+}
